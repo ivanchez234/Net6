a84d6e80c7c120adec590a0aaa0ed456787c6275
Ivan Lukyanov <iukuanovivan@gmail.com>
2026-01-30 00:49:34 +0300

    Initial commit

---
+++ Net1/Net1.cpp
@@ -0,0 +1,30 @@
+#define MODE 1
+#ifndef MODE
+#error Нужно определить MODE
+#endif 
+#if MODE==1
+int add(int a, int b)
+{
+    return a + b;
+}
+#endif
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+#if MODE==0
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE==1
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    int num1, num2;
+    std::cout << "Введите число 1: ";
+    std::cin >> num1;
+    std::cout << "Введите число 2: ";
+    std::cin >> num2;
+    std::cout << "Результат сложения: " << add(num1, num2) << std::endl;
+#else
+#error Invalid number
+#endif
+}
+
